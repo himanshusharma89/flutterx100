e9c8e0dae7425b3e443d4ad88cdf97da895be575
Himanshu Sharma <ranubhardwaj89@gmail.com>
2021-02-16 14:45:41 +0530

    Firebase Initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'dashboard.dart';
 
-void main() {
+void main() async{
+  WidgetsBinding.instance;
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget {
